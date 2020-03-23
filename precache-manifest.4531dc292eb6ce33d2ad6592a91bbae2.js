b36319201d7e3bb34e8730e59789b7c56d4b44e8
rizwanishaq <rizwanishaq@gmail.com>
2020-03-23 18:17:30 +0100

    Updates

---
+++ precache-manifest.4531dc292eb6ce33d2ad6592a91bbae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092191d5795654661453196074fdabef",
+    "url": "/cronavirus/index.html"
+  },
+  {
+    "revision": "d09de9e6ee89724d23f6",
+    "url": "/cronavirus/static/js/2.9f55cb84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cronavirus/static/js/2.9f55cb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a167a9000f03051bfc0",
+    "url": "/cronavirus/static/js/main.dc854e09.chunk.js"
+  },
+  {
+    "revision": "1a5e4ac401a8969d6e99",
+    "url": "/cronavirus/static/js/runtime-main.54e2894e.js"
+  }
+]);
